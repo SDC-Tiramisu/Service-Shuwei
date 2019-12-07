4d5449fbacbfa1b0eff0b1d7fe1be1f84975b68c
Tom Pope <thompope@icloud.com>
2019-12-06 19:42:14 -0800

    Implement image cycling for all recommendations

--- client/App.jsx
+++ client/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
 import styled from 'styled-components';
+import { KeyboardArrowLeft } from 'styled-icons/material/KeyboardArrowLeft';
+import { KeyboardArrowRight } from 'styled-icons/material/KeyboardArrowRight';
 
 const Allrecs = styled.div`
   display: flex;
@@ -27,11 +29,41 @@ const Rec = styled.div`
   box-shadow: 0px 0px 10px grey;
 `;
 
-const Pic = styled.div`
+const Picholder = styled.div`
   display: flex;
+  flex-direction: row;
+  align-items: center;
   height: 220px;
   width: 230px;
-  background-color: black;
+`;
+
+const Picnavleft = styled(KeyboardArrowLeft)`
+  height: 40px;
+  width: 40px;
+  margin-right: -40px;
+  border-radius: 20px;
+  color: white;
+  &:hover {
+    background-color: #101820;
+  }
+`;
+
+const Pic = styled.div`
+  height: 220px;
+  width: 230px;
+  overflow: auto;
+  z-index: -1;
+`;
+
+const Picnavright = styled(KeyboardArrowRight)`
+  height: 40px;
+  width: 40px;
+  margin-left: -40px;
+  border-radius: 20px;
+  color: white;
+  &:hover {
+    background-color: #101820;
+  }
 `;
 
 const Recbody = styled.div`
@@ -112,7 +144,6 @@ class App extends React.Component {
       type: 'GET',
       url: 'api/restaurants/'+restaurantId,
       success: (data) => {
-        console.log('data ', data);
         this.setState({
           genre: data[0].genre,
           title: data[0].title,
@@ -137,20 +168,76 @@ class App extends React.Component {
   }
 };
 
-const Recommendation = (props) => {
-  return (
-    <Rec>
-      <Pic>{props.rec.pics}</Pic>
-      <Recbody>
-        <Rectitle>{props.rec.title}</Rectitle>
-        <Recprice>{props.genre} <span>&#183;</span>  {props.rec.price}</Recprice>
-        <Zagatrated>
-          <Logo><img src="assets/logo.svg"/></Logo>ZAGAT RATED
-        </Zagatrated>
-        <Rectext>{props.rec.text}</Rectext>
-      </Recbody>
-    </Rec>
-  );
+class Recommendation extends App {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      imageIndex: 0
+    };
+
+    this.onLeftClick = this.onLeftClick.bind(this);
+    this.onRightClick = this.onRightClick.bind(this);
+  }
+
+  onLeftClick() {
+    const lastIndex = this.props.rec.pics.length - 1;
+    const { imageIndex } = this.state;
+    const shouldResetIndex = imageIndex === 0;
+    const index =  shouldResetIndex ? lastIndex : imageIndex - 1;
+
+    this.setState({
+      imageIndex: index
+    });
+  }
+
+  onRightClick() {
+    const lastIndex = this.props.rec.pics.length - 1;
+    const { imageIndex } = this.state;
+    const shouldResetIndex = imageIndex === lastIndex;
+    const index =  shouldResetIndex ? 0 : imageIndex + 1;
+
+    this.setState({
+      imageIndex: index
+    });
+  }
+
+  render() {
+    return (
+      <Rec>
+        <Picholder>
+          <Picnavleft onClick={this.onLeftClick}></Picnavleft>
+          <Pic>
+            <ImageSlide url={ this.props.rec.pics[this.state.imageIndex] }/>
+          </Pic>
+          <Picnavright onClick={this.onRightClick}></Picnavright>
+        </Picholder>
+        <Recbody>
+          <Rectitle>{this.props.rec.title}</Rectitle>
+          <Recprice>{this.props.genre} <span>&#183;</span>  {this.props.rec.price}</Recprice>
+          <Zagatrated>
+            <Logo><img src="assets/logo.svg"/></Logo>ZAGAT RATED
+          </Zagatrated>
+          <Rectext>{this.props.rec.text}</Rectext>
+        </Recbody>
+      </Rec>
+    );
+  }
 };
+
+const ImageSlide = ({ url }) => {
+  const styles = {
+    backgroundImage: `url(${url})`,
+    height: '220px',
+    width: '230px',
+    overflow: 'auto',
+    zIndex: '-1'
+  };
+
+  return (
+    <div style={styles}></div>
+  );
+}
 
 ReactDOM.render(<App />, document.getElementById("recommendations"));
