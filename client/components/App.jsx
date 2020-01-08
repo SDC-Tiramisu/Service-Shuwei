f7f50ad5554b672e71c92e99c1ac5b866db1b255
[shuwei] <zhang_s1@gwu.edu>
2020-01-07 17:13:54 -0800

    preparing for ec2

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import $ from 'jquery';
 import styled from 'styled-components';
 import Recommendation from './Recommendation.jsx';
-
+let params = (new URL(document.location)).searchParams;
+let restaurantId = parseInt(params.get('restaurantId')) || 1;
 const Allrecs = styled.div`
   display: flex;
   flex-direction: row;
